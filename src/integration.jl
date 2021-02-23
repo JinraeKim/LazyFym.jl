5d921d08b02c4ff7c4d4e0379a59769a816ab4b0
Jinrae Kim <kjl950403@gmail.com>
2021-02-24 08:47:17 +0900

    Add docs and return version to v0.1.0 (#10)
    * Add docstrings
    
    * WIP
    
    * Return to v0.1.0

--- src/integration.jl
+++ src/integration.jl
@@ -1,9 +1,19 @@
 ## Numerical integration
-# euler method
+"""
+    euler(_ẋ, _x, t, Δt, args...; kwargs...)
+
+Calculate the next state using Euler method.
+`args` and `kwargs` are regarded as constant within the time step.
+"""
 function euler(_ẋ, _x, t, Δt, args...; kwargs...)
     return _x + Δt * _ẋ(_x, t, args...; kwargs...)
 end
-# rk4 method
+"""
+    rk4(_ẋ, _x, t, Δt, args...; kwargs...)
+
+Calculate the next state using Runge-Kutta 4th method.
+`args` and `kwargs` are regarded as constant within the time step.
+"""
 function rk4(_ẋ, _x, t, Δt, args...; kwargs...)
     k1 = _ẋ(_x, t, args...; kwargs...)
     k2 = _ẋ(_x + k1*(0.5*Δt), t + 0.5*Δt, args...; kwargs...)
@@ -11,7 +21,11 @@ function rk4(_ẋ, _x, t, Δt, args...; kwargs...)
     k4 = _ẋ(_x + k3*Δt, t + Δt, args...; kwargs...)
     return _x + (Δt/6)*sum([k1, k2, k3, k4] .* [1, 2, 2, 1])
 end
-# API
+"""
+    ∫(env, ẋ, x, t, Δt, args...; integrator=method, kwargs...)
+
+Perform numerical integration using `method`.
+"""
 function ∫(env, ẋ, x, t, Δt, args...; integrator=rk4, kwargs...)
     # preprocess data everytime may be bad for performance;
     # to improve the simulation speed, you should extend LazyFym functions
--- src/postprocess.jl
+++ src/postprocess.jl
@@ -1,4 +1,16 @@
-# trajs -> NamedTuple
+"""
+    evaluate(trajs)
+
+Convert an array of datum to a concatenated data.
+
+# Examples
+```jldoctest
+julia> using LazyFym
+
+julia> evaluate([(; t=1, x=[1, 2, 3]), (; t=2, x=[2, 3, 4])])
+(t = [1, 2], x = [[1, 2, 3], [2, 3, 4]])
+```
+"""
 function evaluate(trajs)
     _trajs = trajs |> collect
     all_keys = union([keys(traj) for traj in _trajs]...)
@@ -7,16 +19,54 @@ function evaluate(trajs)
     return (; zip(all_keys, all_values)...)  # NamedTuple
 end
 
-# multiple trajs -> NamedTuple (NOTICE: applying `catevaluate` to trajs will give you concatenated NamedTuple)
-function catevaluate(trajs)
-    _trajs = trajs |> collect
-    all_keys = union([keys(traj) for traj in _trajs]...)
-    get_values(key) = vcat([get(traj, key, missing) for traj in _trajs]...)
+"""
+    catevaluate(multiple_trajs)
+
+Concatenate a collection of evaluated trajectories.
+It is probably useful to concatenate the result of parallel simulation.
+Applying `catevaluate` to a single trajectory or
+`evaluate` to multiple trajectories would yield undesirable results.
+
+# Examples
+```jldoctest
+julia> using LazyFym
+
+julia> catevaluate([(t = [1, 2], x = [[1, 2, 3], [2, 3, 4]]), (t = [3, 4], x = [[3, 4, 5], [4, 5, 6]])])
+(t = [1, 2, 3, 4], x = [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6]])
+
+julia> catevaluate([(; t=1, x=[1, 2, 3]), (; t=2, x=[2, 3, 4])])
+(t = [1, 2], x = [1, 2, 3, 2, 3, 4])
+
+julia> evaluate([(t = [1, 2], x = [[1, 2, 3], [2, 3, 4]]), (t = [3, 4], x = [[3, 4, 5], [4, 5, 6]])])
+(t = [[1, 2], [3, 4]], x = [[[1, 2, 3], [2, 3, 4]], [[3, 4, 5], [4, 5, 6]]])
+```
+"""
+function catevaluate(multiple_trajs)
+    _multiple_trajs = multiple_trajs |> collect
+    all_keys = union([keys(trajs) for trajs in _multiple_trajs]...)
+    get_values(key) = vcat([get(trajs, key, missing) for trajs in _multiple_trajs]...)
     all_values = all_keys |> Map(get_values)
     return (; zip(all_keys, all_values)...)  # NamedTuple
 end
 
-# array in array -> array (the first dimension is added)
+"""
+    sequentialise(data)
+
+Convert an array of arrays to a concatenated array throught the first dimension.
+
+It would be useful when plotting figures using data obtained from Transducers.
+
+# Examples
+```jldoctest
+julia> using LazyFym
+
+julia> sequentialise([[1, 2], [3, 4], [5, 6]])
+3×2 Array{Int64,2}:
+ 1  2
+ 3  4
+ 5  6
+```
+"""
 function sequentialise(data)
     return vcat([reshape(data[i], (1, size(data[i])...)) for i in 1:length(data)]...)
 end
