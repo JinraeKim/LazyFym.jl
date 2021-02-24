50c9c9f94e5d7e9a3b4d5f51190077706caa366a
JinraeKim <kjl950403@gmail.com>
2021-02-25 08:53:07 +0900

    WIP

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Transducers
 using InfiniteArrays
 using StructArrays
 using Random
-using Plots
+using Plots, LaTeXStrings
 
 
 function ẋ(env::LazyFym.InputAffineQuadraticCostEnv, x, t)
@@ -16,7 +16,7 @@ _env = LazyFym.InputAffineQuadraticCostEnv()
 command(env, x) = LazyFym.u_optimal(_env, x)  # you can customise it
 
 function initial_condition(env::LazyFym.InputAffineQuadraticCostEnv)
-    return rand(2)
+    return 2*(rand(2) .- 0.5)
 end
 
 function postprocess(env::LazyFym.InputAffineQuadraticCostEnv)
@@ -40,8 +40,10 @@ function lazy()
     traj_x0 = sim(x0)
     data = traj_x0(t1)
     l = @layout [a b]
-    p_x = plot(data.t, data.x |> sequentialise, seriestype=:scatter, label=["x1" "x2"])
-    p_u = plot(data.t, data.u, seriestype=:scatter, label=["u"])
+    p_x = plot(data.t, data.x |> sequentialise,
+        seriestype=:scatter, color=[:red :blue], xlabel=L"t", label=[L"x_{1}" L"x_{2}"], ylim=(-3, 3))
+    p_u = plot(data.t, data.u,
+        seriestype=:scatter, color=[:magenta], xlabel=L"t", label=L"u", ylim=(-3, 3))
     p = plot(p_x, p_u, layout = l)
     savefig(p, "figures/lazy.png")
 end
@@ -60,8 +62,10 @@ function parallel()
     data_parallel = x0s |> Map(x0 -> traj(x0)) |> tcollect
     data_parallel_whole = data_parallel |> Cat() |> StructArray 
     l = @layout [a b]
-    p_x = plot(data_parallel_whole.t, data_parallel_whole.x |> sequentialise, seriestype=:scatter, label=["x1" "x2"])
-    p_u = plot(data_parallel_whole.t, data_parallel_whole.u, seriestype=:scatter, label=["u"])
+    p_x = plot(data_parallel_whole.t, data_parallel_whole.x |> sequentialise,
+        color=[:red :blue], seriestype=:scatter, xlabel=L"t", label=[L"x_{1}" L"x_{2}"], ylim=(-3, 3))
+    p_u = plot(data_parallel_whole.t, data_parallel_whole.u,
+        color=[:magenta], seriestype=:scatter, xlabel=L"t", label=L"u", ylim=(-3, 3))
     p = plot(p_x, p_u, layout = l)
     savefig(p, "figures/parallel.png")
 end
