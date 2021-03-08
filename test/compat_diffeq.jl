a67db7eebe7a4961b53ffd4741f5d8fbe0529467
JinraeKim <kjl950403@gmail.com>
2021-03-08 23:37:28 +0900

    WIP

--- test/compat_diffeq.jl
+++ test/compat_diffeq.jl
@@ -2,6 +2,8 @@ using LazyFym
 using DifferentialEquations
 using Plots
 using Transducers
+using InfiniteArrays
+using LinearAlgebra
 
 struct Env1 <: Fym
 end
@@ -19,28 +21,31 @@ struct NestedEnv <: Fym
 end
 LazyFym.initial_condition(env::Env1) = 1
 LazyFym.initial_condition(env::Env21) = rand(5, 3)
-LazyFym.initial_condition(env::Env22) = rand(100)
+LazyFym.initial_condition(env::Env22) = rand(10)
 nestedenv = NestedEnv(Env1(), Env2(Env21(), Env22()))
 x0 = LazyFym.initial_condition(nestedenv)
 _x0 = LazyFym.raw(nestedenv, x0)
 
-function f(x, t)
-    dx1 = -x.env1
-    dx21 = -x.env2.env21
-    dx22 = -x.env2.env22
+function f(x, p, t)
+    dx1 = -p[1]*x.env1
+    dx21 = -p[2]*x.env2.env21
+    dx22 = -p[3]*x.env2.env22
     (; env1 = dx1, env2 = (; env21 = dx21, env22 = dx22))
 end
 env_index_nt, env_size_nt = LazyFym.preprocess(nestedenv, x0)
-_f(_x, p, t) = LazyFym.raw(nestedenv, f(LazyFym.process(_x, env_index_nt, env_size_nt), t))
+_f(_x, p, t) = LazyFym.raw(nestedenv, f(LazyFym.process(_x, env_index_nt, env_size_nt), p, t))
 tspan = (0.0, 100.0)
 prob = ODEProblem(_f, _x0, tspan)
-@time sol = solve(prob, Tsit5(), saveat=0:0.01:100)
+p0 = [1.0, 2, 3]
+dosetimes = 0:0.01:tspan[end]
+affect!(integrator) = integrator.p = integrator.p .+ 1e-3*integrator.t
+cb_param_update = PresetTimeCallback(dosetimes, affect!)
+saved_values = SavedValues(Float64, NamedTuple)
+cb_save = SavingCallback((u, t, integrator) -> (; u=u, p=integrator.p), saved_values, saveat=0:0.01:100)
+terminate_condition(u, t, integrator) = norm(u) - 1e-3  # == 0
+terminate_affect!(integrator) = terminate!(integrator)
+cb_terminate = ContinuousCallback(terminate_condition, terminate_affect!)
+cbset = CallbackSet(cb_param_update, cb_save, cb_terminate)
+@time sol = solve(prob, Tsit5(); p=p0, callback=cbset)
 xs = sol.u |> Map(_x -> LazyFym.process(_x, env_index_nt, env_size_nt)) |> collect
-nothing
-
-# f(x, p, t) = 1.01 * x
-# x0 = rand(4, 2)
-# tspan = (0.0, 1.0)
-# prob = ODEProblem(f, x0, tspan)
-# sol = solve(prob, Tsit5())
-# plot(sol)
+plot(sol)
