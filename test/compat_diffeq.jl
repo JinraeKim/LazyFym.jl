55a7b9a9ff66f781b3b45b83df589dc6923667d3
JinraeKim <kjl950403@gmail.com>
2021-03-09 08:41:36 +0900

    WIp

--- test/compat_diffeq.jl
+++ test/compat_diffeq.jl
@@ -19,6 +19,9 @@ struct NestedEnv <: Fym
     env1::Env1
     env2::Env2
 end
+function postprocess(x)
+    (; x=x)
+end
 LazyFym.initial_condition(env::Env1) = rand(2)
 LazyFym.initial_condition(env::Env21) = rand(5, 3)
 LazyFym.initial_condition(env::Env22) = rand(10)
@@ -47,16 +50,12 @@ _f(_x, p, t) = LazyFym.raw(nestedenv, f(LazyFym.process(_x, env_index_nt, env_si
 tspan = (0.0, 100.0)
 prob = ODEProblem(_f, _x0, tspan)
 p0 = [1.0, 2, 3]
-dosetimes = 0:0.01:tspan[end]
-affect!(integrator) = integrator.p = integrator.p .+ 1e-3*integrator.t
-cb_param_update = PresetTimeCallback(dosetimes, affect!)
 saved_values = SavedValues(Float64, NamedTuple)
 cb_save = SavingCallback((u, t, integrator) -> (; u=u, p=integrator.p), saved_values, saveat=0:0.01:100)
 terminate_condition(u, t, integrator) = norm(u) - 1e-3  # == 0
 terminate_affect!(integrator) = terminate!(integrator)
 cb_terminate = ContinuousCallback(terminate_condition, terminate_affect!)
-cbset = CallbackSet(cb_param_update, cb_save, cb_terminate)
-# @time sol = solve(prob, Tsit5(); p=p0, callback=cbset)
-@time sol = solve(prob, Tsit5(); p=p0)
-xs = sol.u |> Map(_x -> LazyFym.process(_x, env_index_nt, env_size_nt)) |> collect
+cb_set = CallbackSet(cb_save, cb_terminate)
+@time sol = solve(prob, Tsit5(); p=p0, callback=cb_set)
+@time xs = sol.u |> Map(_x -> LazyFym.process(_x, env_index_nt, env_size_nt)) |> Map(postprocess) |> collect
 plot(sol)
